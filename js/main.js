298d5caf7f4f22f1bc66e8196caec00364050191
Troll223 <64144364+Troll223@users.noreply.github.com>
2020-06-29 20:52:19 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('a[href^="#"]').click(function (event) {
+    var id_clicked_element = $(this).attr('href');
+    var destination = $(id_clicked_element).offset().top;
+    $('html, body').animate({
+        scrollTop: destination
+    }, 1000);
+    return false;
+});
